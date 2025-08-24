38b78e26ad7331f789b05c2e8f6cc4164654232a
fardeen-autonomize <fardeen.khan@autonomize.ai>
2025-08-24 18:03:03 +0530

    W13 L4

---
+++ user-service/src/main/java/com/codingshuttle/learn_kafka/user_service/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.learn_kafka.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka.topic.user-random-topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@PathVariable String message) {
+        for (int i = 0; i < 1000; i++) {
+            kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, "" + i % 2, message + i);
+        }
+
+        return ResponseEntity.ok("Message queued.");
+    }
+}
